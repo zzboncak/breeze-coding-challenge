146bfc1d08dac1e0bf760853d8138d1e3d54dc3d
Zachary Zboncak <zachary.zboncak@gmail.com>
2020-06-23 01:24:57 -0500

    added test suite for the app

--- src/App.test.js
+++ src/App.test.js
@@ -3,43 +3,64 @@ import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
 import App from './App';
 
-
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-    ReactDOM.unmountComponentAtNode(div);
-});
-
-it('should match the snapshot with no files to upload', () => {
-    const wrapper = shallow(<App />);
-
-    expect(wrapper).toMatchSnapshot();
-});
-
-it('buttons should be disabled without a file uploaded', () => {
-    const wrapper = shallow(<App />);
-
-    let button1 = wrapper.find('button').at(0);
-    let button2 = wrapper.find('button').at(1);
-
-    expect(button1.prop('disabled')).toEqual('You need to attach a file to upload');
-    expect(button2.prop('disabled')).toEqual('You need to attach a file to upload');
-});
-
-it('button should stay disabled if bad data is present in file', () => {
-    const wrapper = shallow(<App />);
-
-    wrapper.setState({
-        peopleToImport: [{
-            data: {
-                "first_name": "Zachary",
-                "last_name": "Zboncak",
-                // missing required fields
-            }
-        }]
+describe('<App />', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<App />, div);
+        ReactDOM.unmountComponentAtNode(div);
     });
 
-    let button1 = wrapper.find('button').at(0); // select the button to import people
+    it('should match the snapshot with no files to upload', () => {
+        const wrapper = shallow(<App />);
 
-    expect(button1.prop('disabled')).toEqual('You are missing the header email_address in your CSV file. Please adjust your data and try again.');
-})
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it('buttons should be disabled without a file uploaded', () => {
+        const wrapper = shallow(<App />);
+
+        let button1 = wrapper.find('button').at(0);
+        let button2 = wrapper.find('button').at(1);
+
+        expect(button1.prop('disabled')).toEqual('You need to attach a file to upload');
+        expect(button2.prop('disabled')).toEqual('You need to attach a file to upload');
+    });
+
+    it('button should stay disabled if bad data is present in file', () => {
+        const wrapper = shallow(<App />);
+
+        wrapper.setState({
+            peopleToImport: [{
+                data: {
+                    "first_name": "Zachary",
+                    "last_name": "Zboncak",
+                    // missing required fields
+                }
+            }]
+        });
+
+        let button1 = wrapper.find('button').at(0); // select the button to import people
+
+        expect(button1.prop('disabled')).toEqual('You are missing the header email_address in your CSV file. Please adjust your data and try again.');
+    });
+
+    it('button should enable when csv uploads and data is good', () => {
+        const wrapper = shallow(<App />);
+
+        wrapper.setState({
+            peopleToImport: [{
+                data: {
+                    "first_name": "Zachary",
+                    "last_name": "Zboncak",
+                    "email_address": "zachary.zboncak@gmail.com",
+                    "status": "active"
+                }
+            }]
+        });
+
+        let button1 = wrapper.find('button').at(0);
+
+        console.log(button1.prop('disabled'));
+        expect(button1.prop('disabled')).toEqual('');
+    });
+});
