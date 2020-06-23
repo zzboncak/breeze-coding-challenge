424b1596b3150d2bc400747357593076208388dd
Zachary Zboncak <zachary.zboncak@gmail.com>
2020-06-23 01:03:56 -0500

    added some basic tests for the app component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-// import ReactDOM from "react-dom";
 import { Container, Header } from "semantic-ui-react";
 
 import CSVReader1 from './CSVReader';
--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import App from './App';
 
 
@@ -8,4 +8,20 @@ it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
     ReactDOM.unmountComponentAtNode(div);
+});
+
+it('should match the snapshot with no files to upload', () => {
+    const wrapper = shallow(<App />);
+
+    expect(wrapper).toMatchSnapshot();
+});
+
+it('buttons should be disabled without a file uploaded', () => {
+    const wrapper = shallow(<App />);
+
+    let button1 = wrapper.find('button').at(0);
+    let button2 = wrapper.find('button').at(1);
+
+    expect(button1.prop('disabled')).toEqual('You need to attach a file to upload');
+    expect(button2.prop('disabled')).toEqual('You need to attach a file to upload');
 });
